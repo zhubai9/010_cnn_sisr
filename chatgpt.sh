566655f512ed84cb9a973d557a4defc4f29e3723
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2021-02-25 20:18:00 +0000

    Ingénieur système et réseau confirmé (H/F) en CDI à Paris

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 GLOBIGNORE="*"
 
-CHAT_INIT_PROMPT="You are ChatGPT, a Large Language Model trained by OpenAI. You will be answering questions from users. You answer as concisely as po
+CHAT_INIT_PROMPT="You are ChatGPT, a Large Language Model trained by OpenAI. You will be answering questions from users. You answer as concisely as possible for e
