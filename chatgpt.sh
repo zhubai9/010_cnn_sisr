0c2b55b8ddfe8ea6ff0b21eead53d94880f3f359
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2021-02-25 10:18:32 +0000

    Performance improvements

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 GLOBIGNORE="*"
 
-CHAT_INIT_PROMPT="You are ChatGPT, a Large Language Model trained by OpenAI. You will
+CHAT_INIT_PROMPT="You are ChatGPT, a Large Language Model trained by OpenAI. You will be answering q
