de2833fb4f427f51ef6266e3ae3cb4eda2bc188c
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2021-02-25 17:21:55 +0000

    Fixed test bugs.

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 GLOBIGNORE="*"
 
-CHAT_INIT_PROMPT="You are ChatGPT, a Large Language Model trained by OpenAI. You will be answering questions from users. You answer as 
+CHAT_INIT_PROMPT="You are ChatGPT, a Large Language Model trained by OpenAI. You will be answering questions from users. You answer as concisely as po
