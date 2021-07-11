04abdb24d4d9736f608b96cce4bc37d96fd32767
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2021-07-11 21:28:40 +0000

    Small doc update

--- chatgpt.sh
+++ chatgpt.sh
@@ -30,4 +30,4 @@ request_to_completions() {
   			"model": "'"$MODEL"'",
   			"prompt": "'"${request_prompt}"'",
   			"max_tokens": '$MAX_TOKENS',
-  			"temperature":
+  			"temperature": '$TEMPERAT
