2ea8f7af77704c0b0f6a2145d5f4c22f8387e408
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2021-08-22 10:47:41 +0000

    let's try this

--- chatgpt.sh
+++ chatgpt.sh
@@ -53,4 +53,6 @@ request_to_image() {
 # $1 should be the message(s) formatted with role and content
 request_to_chat() {
 	message="$1"
-	response=$(curl https://api.openai.com/v1/chat/complet
+	response=$(curl https://api.openai.com/v1/chat/completions \
+		-sS \
+	
