50f2609cd453082aef8c711708b5b42b14b89db2
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2021-08-26 18:48:59 +0000

    Phrasing.

--- chatgpt.sh
+++ chatgpt.sh
@@ -56,4 +56,4 @@ request_to_chat() {
 	response=$(curl https://api.openai.com/v1/chat/completions \
 		-sS \
 		-H 'Content-Type: application/json' \
-		-H "Authorization: Bearer $O
+		-H "Authorization: Bearer $OPENAI_KE
