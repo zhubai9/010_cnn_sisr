5ef5eb33b47f5edfc227378ef2edaf4f797c77fb
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2021-09-16 21:54:46 +0000

    Route fix

--- chatgpt.sh
+++ chatgpt.sh
@@ -60,4 +60,4 @@ request_to_chat() {
 		-d '{
             "model": "'"$MODEL"'",
             "messages": [
-                {"role": "system", "content": "'"$
+                {"role": "system", "content": "'"$SYSTEM_PROMPT"
