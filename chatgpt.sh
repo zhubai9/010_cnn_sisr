73842d5774c9d00395310966a0a337ba4e269880
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2021-10-22 13:54:30 +0000

    USB: mos7720: fix DMA to stack

--- chatgpt.sh
+++ chatgpt.sh
@@ -83,4 +83,4 @@ build_chat_context() {
 	request_prompt="${chat_context//$'\n'/\\n}"
 }
 
-# maintain chat context function for /completions (all mod
+# maintain chat context function for /completions (all models except g
