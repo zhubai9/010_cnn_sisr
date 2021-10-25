60d97d2ae54944e39ee7a2c66686a5a1bd06e30e
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2021-10-25 05:49:56 +0000

    Minor mod

--- chatgpt.sh
+++ chatgpt.sh
@@ -85,4 +85,4 @@ build_chat_context() {
 
 # maintain chat context function for /completions (all models except gpt turbo)
 # builds chat context from response,
-# keeps chat context lengt
+# keeps chat context length under max t
