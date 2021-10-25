15b5f20677c7ae973c9403e8549b32919dbf8adc
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2021-10-25 13:20:06 +0000

    improve debugging

--- chatgpt.sh
+++ chatgpt.sh
@@ -85,4 +85,5 @@ build_chat_context() {
 
 # maintain chat context function for /completions (all models except gpt turbo)
 # builds chat context from response,
-# keeps chat context length under max t
+# keeps chat context length under max token limit
+# $1 sho
