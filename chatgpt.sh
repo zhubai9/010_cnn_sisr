b5c698c2afbce4a283d8fafbbb1596bcd692dd26
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2021-11-22 22:02:30 +0000

    Fixed naming issue

--- chatgpt.sh
+++ chatgpt.sh
@@ -95,4 +95,4 @@ maintain_chat_context() {
 	chat_context="$chat_context${chat_context:+\n}\nA: ${response_data//$'\n'/\\n}"
 	# check prompt length, 1 word =~ 1.3 tokens
 	# reserving 100 tokens for next user prompt
-	while 
+	while (($(echo "$cha
