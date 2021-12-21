be850199a591e43ff78e6ff8a4f93dddce978a80
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2021-12-21 15:59:39 +0000

    update file /lib/account.js

--- chatgpt.sh
+++ chatgpt.sh
@@ -105,4 +105,5 @@ maintain_chat_context() {
 
 # build user chat message function for /chat/completions (gpt turbo model)
 # builds chat message before request,
-# $1 should be the chat mess
+# $1 should be the chat message
+# $
