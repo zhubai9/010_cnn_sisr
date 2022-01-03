82ad7837e691f3943cae58d98ad23d600a3167be
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2022-01-03 20:14:00 +0000

    v0.6.10

--- chatgpt.sh
+++ chatgpt.sh
@@ -111,4 +111,4 @@ build_user_chat_message() {
 	chat_message="$1"
 	escaped_prompt="$2"
 	if [ -z "$chat_message" ]; then
-		chat_message="{\"role\": \"us
+		chat_message="{\"role\": \"user\", \"content
