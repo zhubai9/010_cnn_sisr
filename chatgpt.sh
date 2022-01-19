f5792e33a995f9bc49b7fa58a71b84fe63c40b97
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2022-01-19 13:26:26 +0000

    add lock file

--- chatgpt.sh
+++ chatgpt.sh
@@ -122,4 +122,5 @@ build_user_chat_message() {
 # adds the assistant response to the message in (chatml) format
 # for /chat/completions (gpt turbo model)
 # keeps messages length under max token limit
-# $1 should be the chat messa
+# $1 should be the chat message
+# $2 shoul
