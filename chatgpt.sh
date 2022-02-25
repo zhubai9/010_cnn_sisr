cfb6d4b2518c655efbaae1e5331fffe9eb8c6b4b
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2022-02-25 15:50:07 +0000

    NFSv4: Fix lock recovery when CREATE_SESSION/SETCLIENTID_CONFIRM fails

--- chatgpt.sh
+++ chatgpt.sh
@@ -133,4 +133,4 @@ add_assistant_response_to_chat_message() {
 	# add response to chat context as answer
 	chat_message="$chat_message, {\"role\": \"assistant\", \"content\": \"$response_data\"}"
 
-	# transform to json array to
+	# transform to json array to parse wit
