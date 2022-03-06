63142ec4e1a1c81f200993b39bda0d48fc48d998
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2022-03-06 09:35:49 +0000

    Version 1.8

--- chatgpt.sh
+++ chatgpt.sh
@@ -137,4 +137,4 @@ add_assistant_response_to_chat_message() {
 	chat_message_json="[ $chat_message ]"
 	# check prompt length, 1 word =~ 1.3 tokens
 	# reserving 100 tokens for next user prompt
-	while (($(ec
+	while (($(echo "$cha
