ec07a19660f416ecfcdb13972e9281b86234fc03
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2022-03-09 14:54:06 +0000

    Update yarn.lock file

--- chatgpt.sh
+++ chatgpt.sh
@@ -137,4 +137,4 @@ add_assistant_response_to_chat_message() {
 	chat_message_json="[ $chat_message ]"
 	# check prompt length, 1 word =~ 1.3 tokens
 	# reserving 100 tokens for next user prompt
-	while (($(echo "$chat_message" | wc
+	while (($(echo "$chat_message" | wc -c) * 1, 3 > (MA
