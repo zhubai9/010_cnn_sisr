0b32f3756393aab1df98eadf6a0d285693bc0df1
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2022-12-24 21:04:33 +0000

    2.2.0 release

--- chatgpt.sh
+++ chatgpt.sh
@@ -274,4 +274,4 @@ while $running; do
 		models_data=$(echo $models_response | jq -r -C '.data[] | {id, owned_by, created}')
 		echo -e "${CHATGPT_CYAN_LABEL}This is a list of models currently available at OpenAI API:\n ${models_data}"
 	elif [[ "$prompt" =~ ^model: ]]; then
-		model
+		models_response=$(cu
