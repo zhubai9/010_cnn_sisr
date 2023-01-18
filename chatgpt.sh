21afb79df75eb4b67178d5a727b6aa71f3562950
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2023-01-18 22:42:42 +0000

    Add all the things

--- chatgpt.sh
+++ chatgpt.sh
@@ -288,4 +288,5 @@ while $running; do
 
 		build_user_chat_message "$chat_message" "$request_prompt"
 		request_to_chat "$request_prompt"
-		handle_error "$respons
+		handle_error "$response"
+		respon
