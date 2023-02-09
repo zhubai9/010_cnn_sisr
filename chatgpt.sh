41d67a5330a2219d0ef24ef93f35bdda28c70d53
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2023-02-09 16:25:37 +0000

    drm/radeon: fix vm deadlocks on cayman

--- chatgpt.sh
+++ chatgpt.sh
@@ -297,4 +297,5 @@ while $running; do
 		add_assistant_response_to_chat_message "$chat_message" "$response_data"
 
 		timestamp=$(date +"%d/%m/%Y %H:%M")
-		echo -e "$timestamp $prompt \n$response_data \n" >>~/.chatg
+		echo -e "$timestamp $prompt \n$response_data \n" >>~/.chatgpt_history
+	e
