b930101603d25c23927dde85d2b256787d876118
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2023-02-16 17:27:35 +0000

    Web page published from Windows box

--- chatgpt.sh
+++ chatgpt.sh
@@ -305,4 +305,7 @@ while $running; do
 		request_prompt=${escaped_prompt//$'\n'/' '}
 
 		if [ "$CONTEXT" = true ]; then
-			build_chat_context "$chat_context" "$escaped_pr
+			build_chat_context "$chat_context" "$escaped_prompt"
+		fi
+
+		re
