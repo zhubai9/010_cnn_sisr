c6d402b904f09cdce8acc7786db345630fa8ef99
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2023-03-01 19:07:39 +0000

    [ci skip] update README

--- chatgpt.sh
+++ chatgpt.sh
@@ -319,4 +319,6 @@ while $running; do
 		fi
 
 		timestamp=$(date +"%d/%m/%Y %H:%M")
-		echo -e "$timestamp $prompt \n$escaped_response_data \n" >>~/.chatg
+		echo -e "$timestamp $prompt \n$escaped_response_data \n" >>~/.chatgpt_history
+	fi
+d
