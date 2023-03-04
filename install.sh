ea409f2b2b5af3e06d70d37d66fa16d7bd2b75e9
zhubai9 <110256061+zhubai9@users.noreply.github.com>
2023-03-04 11:07:38 +0000

    Address review feedback

--- install.sh
+++ install.sh
@@ -30,3 +30,17 @@ fi
 
 # Installing chatgpt script
 curl -sS https://raw.githubusercontent.com/0xacx/chatGPT-shell-cli/main/chatgpt.sh -o /usr/local/bin/chatgpt
+
+# Replace open image command with xdg-open for linux systems
+if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "freebsd"* ]]; then
+  sed -i 's/open "\${image_url}"/xdg-open "\${image_url}"/g' '/usr/local/bin/chatgpt'
+fi
+chmod +x /usr/local/bin/chatgpt
+echo "Installed chatgpt script to /usr/local/bin/chatgpt"
+
+read -p "Please enter your OpenAI API key: " key
+
+# Adding OpenAI key to shell profile
+# zsh profile
+if [ -f ~/.zprofile ]; then
+  echo "export OPENAI_KEY=$key" >>~/.zprofile
